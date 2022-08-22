ba907eca10c730b4458b2d5fef8f739e1b145d6f
Piotr Gaczkowski <DoomHammerNG@gmail.com>
2022-08-22 11:30:52 +0000

    Switch to SpaceAPI

--- bot.py
+++ bot.py
@@ -30,33 +30,40 @@ logging.basicConfig(level=logging.INFO)
 
 client = discord.Client()
 
+async def update_state(state):
+    if client.user:
+        logging.info(f'Updating the presence to "{state}"')
+        await client.change_presence(activity=discord.Activity(name=f"the Space (*{state}*)", type=discord.ActivityType.watching))
+        try:
+            await client.user.edit(avatar=avatars[state])
+        except:
+            logging.error(traceback.format_exc())
+        for guild in client.guilds:
+            member = guild.get_member_named(client.user.name)
+            await member.edit(nick=usernames[state])
+
 async def update_presence(state):
     global current_state
     if state != current_state:
         current_state = state
-        if client.user:
-            logging.info(f'Updating the presence to "{state}"')
-            await client.change_presence(activity=discord.Activity(name=f"the Space (*{state}*)", type=discord.ActivityType.watching))
-            try:
-                await client.user.edit(avatar=avatars[state])
-            except:
-                logging.error(traceback.format_exc())
-            for guild in client.guilds:
-                member = guild.get_member_named(client.user.name)
-                await member.edit(nick=usernames[state])
+        await update_state(state)
 
 # Fire every minute
 @aiocron.crontab('* * * * *')
 async def is_there_life_on_mars():
     logging.info('Checking the status')
-    space_state = requests.get(space_edpoint).json()['status']
+    space_state = requests.get(space_edpoint).json()['state']['open']
+    if space_state:
+        space_state = 'open'
+    else:
+        space_state = 'closed'
     logging.info(f'Current status: {space_state}')
     await update_presence(space_state)
 
 @client.event
 async def on_ready():
     for guild in client.guilds:
-        logging.info(f'{client.user} has connected to Discord server {guild}!')
+        logging.info(f'{client.user} has connected to Discord server {guild} using endpoint {space_edpoint}!')
     for state in ['closed', 'open']:
         with open(f'res/glider_{state}.png', 'rb') as avatar:
             avatars[state] = avatar.read()
